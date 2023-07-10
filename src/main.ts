d315098b1f52e30befd98bc35f6e721b0674676c
ravikiran0357 <108070096+ravikiran0357@users.noreply.github.com>
2023-07-10 20:10:35 +0530

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
   template: `
     <h1>Hello from {{name}}!</h1>
     <a target="_blank" href="https://angular.io/start">
-      Learn more about Angular 
+      Learn more about Angular de
     </a>
   `,
 })
